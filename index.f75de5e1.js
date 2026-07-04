4024cefbf0d5dbcd5f729f0f4bd6565b8aba642a
pavlo <nadazasada@gmail.com>
2026-07-04 13:26:15 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,31 @@
+"use strict";
+function waitFor(element, eventName) {
+    return new Promise((resolve)=>{
+        const myEvent = ()=>{
+            element.removeEventListener(eventName, myEvent);
+            resolve(`It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`);
+        };
+        element.addEventListener(eventName, myEvent);
+    // console.log(myEvent());
+    });
+}
+// console.log(waitFor());
+const printMessage = (message)=>{
+    const div = document.createElement("div");
+    div.className = "message";
+    div.textContent = message;
+    document.body.append(div);
+};
+const loginField = document.getElementById("login");
+const passwordField = document.getElementById("password");
+const button = document.getElementById("submit");
+waitFor(loginField, "click").then(printMessage);
+waitFor(passwordField, "click").then(printMessage);
+waitFor(button, "click").then(printMessage);
+waitFor(loginField, "input").then(printMessage);
+waitFor(passwordField, "input").then(printMessage);
+waitFor(loginField, "blur").then(printMessage);
+waitFor(passwordField, "blur").then(printMessage);
+waitFor(button, "blur").then(printMessage); // It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.
+
+//# sourceMappingURL=index.f75de5e1.js.map
